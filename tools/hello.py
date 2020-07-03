a12e40fcb43b4b19420c9c56302ddcf887ad5a09
DavidKuangGitHub <davidkuang416@gmail.com>
2020-07-03 11:57:27 -0400

    commit and understand that runtime-env should be setup properly

--- tools/hello.py
+++ tools/hello.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 
-print("Hello World! David on Apr 22, 2020")
+print("Hello World! David on Jul 3rd Thu, 2020 and understand runtime-env should be setup properly.")
 
 import matplotlib as mpl  
 print(mpl.get_cachedir())  
